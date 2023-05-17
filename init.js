6c371443191591f18e8052671f53c55bfe31724c
nyanyaonn <nyanyaonn@gmail.com>
2023-05-18 04:07:07 +0800

    invocation

--- init.js
+++ init.js
@@ -1,2 +1,6 @@
-// Create a palette named "Sample Palette" and load the capture.html file
-Acad.Application.addPalette("TOOL LAYOUTING - NTB", "https://alfains.github.io/layout_lisp_/ui/palette.html");
+function run() {
+    // Create a palette named "Sample Palette" and load the capture.html file
+    Acad.Application.addPalette("TOOL LAYOUTING - NTB", "https://alfains.github.io/layout_lisp_/ui/palette.html");
+}
+
+run();
