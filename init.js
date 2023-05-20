a3d229abb3e25434b4e5858e1c3cd76511321834
nyanyaonn <nyanyaonn@gmail.com>
2023-05-21 07:27:17 +0800

    sync online and offline

--- init.js
+++ init.js
@@ -1,19 +1,9 @@
 function run() {
-    // Create a palette named "Sample Palette" and load the capture.html file
-<<<<<<< Updated upstream
-    Acad.Application.addPalette("TOOL LAYOUTING - NTB", "https://alfains.github.io/layout_lisp_/ui/palette.html").then(success,error);
-=======
     Acad.Application.addPalette("TOOL LAYOUTING - NTB", "https://alfains.github.io/layout_lisp_/ui/palette.html");
     localStorage.setItem("lpal", 1);
->>>>>>> Stashed changes
 }
 
-window.addEventListener('beforeunload', (event) => {
-    event.preventDefault();
-    event.returnValue = '';
-    alert("aaaa");
-  });
 
-if(localStorage.getItem("lpal") == 1) {
+if(localStorage.getItem("lpal") != 1) {
     run();
 }
