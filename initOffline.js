6c2cbf6593e391037ea6d52d221cb51132058bdd
nyanyaon <nyanyaonn@gmail.com>
2024-03-21 12:19:17 +0800

    change url js api

--- initOffline.js
+++ initOffline.js
@@ -1,9 +1,11 @@
 function run() {
     // Create a palette named "Sample Palette" and load the capture.html file
-    Acad.Application.addPalette("TOOL LAYOUTING - NTB", "C:/App/layout_lisp_/ui/palette.html");
+    Acad.Application.activedocument.addPalette("TOOL LAYOUTING - NTB", "C:/App/layout_lisp_/ui/palette.html");
     localStorage.setItem("lpal", 1);
 }
 
-if(localStorage.getItem("lpal") == 1) {
-    run();
-}
+// if(localStorage.getItem("lpal") == 1) {
+//     run();
+// }
+
+run();
